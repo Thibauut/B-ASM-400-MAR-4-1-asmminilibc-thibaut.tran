f57417f0c1ac4adce1b3541d2f0d64a7b912f369
Thibauut <91698852+Thibauut@users.noreply.github.com>
2023-02-26 23:04:54 +0100

    Update memset.asm

--- memset.asm
+++ memset.asm
@@ -2,7 +2,7 @@ global memset
 memset:
     xor rcx, rcx ; rcx = 0
 for:
-    ;si rdx est égal à 0 on sort de la boucle
+    ;si rdx est égal à rdx on sort de la boucle
     cmp rcx, rdx
     je return
     ;change le caractère à l'adresse rax
